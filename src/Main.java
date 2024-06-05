b8bd9188c650f881539acbf9c92b48b216206285
Ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-06-05 09:05:01 +0200

    enter-commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package orderlist;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Scanner;
+
+public class ToDoList {
+    static String[] tasks = new String[100];
+    static boolean[] completed = new boolean[100];
+    static int counter = 0;
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int option;
+
+        do {
+            showMenu();
+            option = scanner.nextInt();
+            scanner.nextLine();
+            executeOption(option);
+        } while (option != 5);
+
+        System.out.println("Exiting the application...");
+    }
+
+    static void showMenu() {
+        System.out.println("Welcome to the ToDo List");
+        System.out.println("=========================");
+        System.out.println("1. Add Task");
+        System.out.println("2. Delete Task");
+        System.out.println("3. List Tasks");
+        System.out.println("4. Mark Task as Completed");
+        System.out.println("5. Exit");
+        System.out.print("Select an option: ");
+    }
+
+    static void executeOption(int option) {
+        switch (option) {
+            case 1:
+                addTask();
+                break;
+            case 2:
+                deleteTask();
+                break;
+            case 3:
+                listTasks();
+                break;
+            case 4:
+                markTaskCompleted();
+                break;
+            case 5:
+                break;
+            default:
+                System.out.println("Invalid option. Please try again.");
         }
     }
 }
